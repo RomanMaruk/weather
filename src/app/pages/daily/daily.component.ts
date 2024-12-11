efc9ad0cb37aca5548978bb3421ddb9543be4ea4
Roman Maruk <maruchok@gmail.com>
2024-12-11 12:47:38 +0200

    add loading

--- src/app/pages/daily/daily.component.ts
+++ src/app/pages/daily/daily.component.ts
@@ -1,15 +1,23 @@
 import { AsyncPipe, DecimalPipe, TitleCasePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, map, switchMap } from 'rxjs';
+import { Observable, finalize, map, switchMap, tap } from 'rxjs';
 import { FormatDatePipe } from '../../pipes/format-date.pipe';
 import { ApiService } from '../../services/api.service';
 import { GetIconPipe } from '../../pipes/get-icon.pipe';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-daily',
   standalone: true,
-  imports: [TitleCasePipe, DecimalPipe, AsyncPipe, FormatDatePipe, GetIconPipe],
+  imports: [
+    TitleCasePipe,
+    DecimalPipe,
+    AsyncPipe,
+    FormatDatePipe,
+    GetIconPipe,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './daily.component.html',
   styleUrl: './daily.component.scss',
 })
@@ -17,11 +25,16 @@ export class DailyComponent {
   private apiService = inject(ApiService);
   private activateRouter = inject(ActivatedRoute);
 
+  public loading = false;
+
   public nameCity$ = this.activateRouter.params.pipe(map((p) => p['id']));
 
   public weatherData$: Observable<any> = this.activateRouter.queryParams.pipe(
+    tap(() => (this.loading = true)),
     switchMap(({ lat, lon }) => {
-      return this.apiService.dayly({ lat, lon });
+      return this.apiService
+        .dayly({ lat, lon })
+        .pipe(finalize(() => (this.loading = false)));
     })
   );
 }
